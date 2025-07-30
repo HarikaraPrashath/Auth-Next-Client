ccc969d9d1c0a94f913b129f7417e99ca19cc620
Harigara Prashath <prashathhari2002@gmail.com>
2025-07-30 14:28:19 +0530

    ui change

--- app/page.tsx
+++ app/page.tsx
@@ -55,15 +55,15 @@ export default function Home() {
             </p>
             <div className="flex space-x-1">
               <div
-                className="w-2 h-2 bg-blue-500 rounded-full animate-bounce"
+                className="w-2 h-2 bg-green-800 rounded-full animate-bounce"
                 style={{ animationDelay: "0s" }}
               ></div>
               <div
-                className="w-2 h-2 bg-purple-500 rounded-full animate-bounce"
+                className="w-2 h-2 bg-green-600 rounded-full animate-bounce"
                 style={{ animationDelay: "0.2s" }}
               ></div>
               <div
-                className="w-2 h-2 bg-indigo-500 rounded-full animate-bounce"
+                className="w-2 h-2 bg-indigo-400 rounded-full animate-bounce"
                 style={{ animationDelay: "0.4s" }}
               ></div>
             </div>
